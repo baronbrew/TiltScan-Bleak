51dfa4eb9614ba1b07ace6e3ef84af50aa9c5662
Noah Baron <noahbaron@gmail.com>
2023-03-24 11:00:01 -0700

    Added Async Web Server

--- tilt-scan.py
+++ tilt-scan.py
@@ -1,9 +1,17 @@
 import asyncio
 import os
+import datetime
+import time
+import json
 from bleak import BleakScanner
 from beacontools import parse_packet
 from aiohttp import web
 
+tiltdatalist = []
+tiltdatadict = {}
+
+routes = web.RouteTableDef()
+@routes.get('/macid/all')
 async def handler(request):
   dir_list = os.listdir()
   filter_object = filter(lambda a: 'TILT-' in a, dir_list)
@@ -14,7 +22,8 @@ async def handler(request):
       with open(i) as f:
         lines += str(f.readlines())
   else: print("No Tilt data files found.")
-  return web.Response(text=str(lines))
+  tiltdatalist.append(tiltdatadict)
+  return web.json_response(tiltdatalist)
 
 tiltcolordict = {
 "a495bb10-c5b1-4b44-b512-1370f02d74de" : "RED",
@@ -38,6 +47,7 @@ def detection_callback(device, advertisement_data):
       # only process packets that succesfully parse as iBeacons
       if adv:
        print ("Mac: %s" % device.address)
+       deviceAddress = device.address
        print ("RSSI: %d" % advertisement_data.rssi)
        print("UUID: %s" % adv.uuid)
        print("Major: %d" % adv.major)
@@ -51,38 +61,68 @@ def detection_callback(device, advertisement_data):
         print("Precision: standard")
         print("TempF: %d" % majorfloat)
         print("SG: %.3f" % (minorfloat / 1000))
+        uncalTemp = majorfloat
+        uncalSG = minorfloat / 1000
        else:
         print("Precision: high")
         print("TempF: %.1f" % (majorfloat / 10))
         print("SG: %.4f" % (minorfloat / 10000))
+        uncalTemp = majorfloat / 10
+        uncalSG = minorfloat / 10000
        #Process tx_power byte
        if adv.tx_power == -59:
         print ("Battery Age in Weeks: Updating...")
-        #stop_event.set()
+        battAgeWeeks = 'pending'
+        opStatus = 0
        elif adv.tx_power == -103:
         print ("Battery Age in Weeks: Waking up...")
+        battAgeWeeks = 'pending'
+        opStatus = 'wake up'
        elif adv.tx_power < 0:
          print ("Battery Age in Weeks: %d" % (int(adv.tx_power) + 2 ** 8))
+         battAgeWeeks = int(adv.tx_power) + 2 ** 8
+         opStatus = 1
        else:
          print ("Battery Age in Weeks: %d" % (int(adv.tx_power)))
+         battAgeWeeks = int(adv.tx_power)
+         opStatus = 1
+       # log scanned tilt info to file
        with open(('TILT-' + (str(tiltcolordict.get(adv.uuid)) + '-' + device.address).replace(':','-')) + '.json','w') as f:
         f.write(device.address)
+       # save scanned tilt info to list of json objects
+       global tiltdatadict 
+       tiltdatadict = {
+        "uuid" : adv.uuid,
+        "major" : adv.major,
+        "minor" : adv.minor,
+        "tx_power" : adv.tx_power,
+        "rssi" : advertisement_data.rssi,
+        "mac" : device.address,
+        "Color" : tiltcolordict.get(adv.uuid),
+        "timeStamp" : time.time() * 1000  ,
+        "formatteddate" : datetime.datetime.now().strftime('%x %X'),
+        "Timepoint" : (time.time() / 60 / 60 / 24 + 25569) - time.timezone / 60 / 60 / 24,
+        "uncalSG" : uncalSG,
+        "uncalTemp" : uncalTemp,
+        "battAgeWeeks" : battAgeWeeks,
+        "opStatus" : opStatus
+        }
 
-stop_server = asyncio.Event()
+event_control = asyncio.Event()
 
 async def startWebServer():
   server = web.Server(handler)
   runner = web.ServerRunner(server)
   await runner.setup()
-  site = web.TCPSite(runner, 'localhost', 8080)
+  site = web.TCPSite(runner, 'localhost', 1880)
   await site.start()
   print("======= Serving on http://127.0.0.1:8080/ ======")
-  await stop_server.wait()
+  await event_control.wait()
 
 async def startTiltScanner():
   scanner = BleakScanner(filters={"scanning_mode":"passive"})
   async with BleakScanner(detection_callback) as scanner:
     await startWebServer()
-    await stop_server.wait()
+    await event_control.wait()
 
 asyncio.run(startTiltScanner())     
