0657a3d01e0856609c81fe71f72a7348b84b1aab
Noah Baron <noahbaron@gmail.com>
2023-03-24 22:34:00 -0700

    Added shutdown feature

--- tilt-scan.py
+++ tilt-scan.py
@@ -9,19 +9,17 @@ from aiohttp import web
 tiltdatalist = []
 tiltdatadict = {}
 
-routes = web.RouteTableDef()
-@routes.get('/macid/all')
-async def handler(request):
-  dir_list = os.listdir()
-  filter_object = filter(lambda a: 'TILT-' in a, dir_list)
-  tilt_list = list(filter_object)
-  lines = ''
-  if (tilt_list):
-    for i in tilt_list:
-      with open(i) as f:
-        lines += str(f.readlines())
-  else: print("No Tilt data files found.")
+async def handler1(request):
+  for i in tiltdatalist:
+    # find and remove Tilts not seen in the last 2 minutes
+    if i['timeStamp'] + 120000 < time.time() * 1000:
+      tiltdatalist.remove(i)
   return web.json_response(tiltdatalist)
+
+event_control = asyncio.Event()
+async def handler2(request):
+  event_control.set()
+  return web.Response(text='shutting down')
 
 tiltcolordict = {
 "a495bb10-c5b1-4b44-b512-1370f02d74de" : "RED",
@@ -44,58 +42,61 @@ def detection_callback(device, advertisement_data):
       adv = parse_packet(beaconbytes)
       # only process packets that succesfully parse as iBeacons
       if adv:
-       """
-       print ("Mac: %s" % device.address)
-       print ("RSSI: %d" % advertisement_data.rssi)
-       print("UUID: %s" % adv.uuid)
-       print("Major: %d" % adv.major)
-       print("Minor: %d" % adv.minor)
-       print("TX Power: %d" % adv.tx_power)
-       """
-       #Tilt specific processing
-       #print("Color: %s" % tiltcolordict.get(adv.uuid))
-       majorfloat = float(adv.major)
-       minorfloat = float(adv.minor)
-       if adv.minor < 5000:
+       if tiltcolordict.get(adv.uuid):
+        # only process packets that succesfully parse as Tilts
+        #print (tiltcolordict.get(adv.uuid))
         """
-        print("Precision: standard")
-        print("TempF: %d" % majorfloat)
-        print("SG: %.3f" % (minorfloat / 1000))
+        print ("Mac: %s" % device.address)
+        print ("RSSI: %d" % advertisement_data.rssi)
+        print("UUID: %s" % adv.uuid)
+        print("Major: %d" % adv.major)
+        print("Minor: %d" % adv.minor)
+        print("TX Power: %d" % adv.tx_power)
         """
-        uncalTemp = majorfloat
-        uncalSG = minorfloat / 1000
-       else:
-        """
-        print("Precision: high")
-        print("TempF: %.1f" % (majorfloat / 10))
-        print("SG: %.4f" % (minorfloat / 10000))
-        """
-        uncalTemp = majorfloat / 10
-        uncalSG = minorfloat / 10000
-       # Process tx_power byte
-       if adv.tx_power == -59:
-        #print ("Battery Age in Weeks: Updating...")
-        battAgeWeeks = 'pending'
-        opStatus = 0
-       elif adv.tx_power == -103:
-        #print ("Battery Age in Weeks: Waking up...")
-        battAgeWeeks = 'pending'
-        opStatus = 'wake up'
-       elif adv.tx_power < 0:
-         #print ("Battery Age in Weeks: %d" % (int(adv.tx_power) + 2 ** 8))
-         battAgeWeeks = int(adv.tx_power) + 2 ** 8
-         opStatus = 1
-       else:
-         #print ("Battery Age in Weeks: %d" % (int(adv.tx_power)))
-         battAgeWeeks = int(adv.tx_power)
-         opStatus = 1
-       # log scanned tilt info to file
-       with open(('TILT-' + (str(tiltcolordict.get(adv.uuid)) + '-' + device.address).replace(':','-')) + '.json','w') as f:
-        f.write(device.address)
-       # save scanned tilt info to list of json objects
-       global tiltdatadict 
-       tiltdatadict = {
-        "uuid" : adv.uuid,
+        #Tilt specific processing
+        #print("Color: %s" % tiltcolordict.get(adv.uuid))
+        majorfloat = float(adv.major)
+        minorfloat = float(adv.minor)
+        if adv.minor < 5000:
+          """
+          print("Precision: standard")
+          print("TempF: %d" % majorfloat)
+          print("SG: %.3f" % (minorfloat / 1000))
+          """
+          uncalTemp = majorfloat
+          uncalSG = minorfloat / 1000
+        else:
+          """
+          print("Precision: high")
+          print("TempF: %.1f" % (majorfloat / 10))
+          print("SG: %.4f" % (minorfloat / 10000))
+          """
+          uncalTemp = majorfloat / 10
+          uncalSG = minorfloat / 10000
+          # Process tx_power byte
+        if adv.tx_power == -59:
+          #print ("Battery Age in Weeks: Updating...")
+          battAgeWeeks = 'pending'
+          opStatus = 0
+        elif adv.tx_power == -103:
+          #print ("Battery Age in Weeks: Waking up...")
+          battAgeWeeks = 'pending'
+          opStatus = 'wake up'
+        elif adv.tx_power < 0:
+          #print ("Battery Age in Weeks: %d" % (int(adv.tx_power) + 2 ** 8))
+          battAgeWeeks = int(adv.tx_power) + 2 ** 8
+          opStatus = 1
+        else:
+          #print ("Battery Age in Weeks: %d" % (int(adv.tx_power)))
+          battAgeWeeks = int(adv.tx_power)
+          opStatus = 1
+        formatteddate = datetime.datetime.now().strftime('%x %X')
+        Timepoint = (time.time() / 60 / 60 / 24 + 25569) - time.timezone / 60 / 60 / 24
+        
+        # save scanned tilt info to list of json objects
+        global tiltdatadict
+        tiltdatadict = {
+        "uuid" : adv.uuid.replace('-',''),
         "major" : adv.major,
         "minor" : adv.minor,
         "tx_power" : adv.tx_power,
@@ -103,32 +104,62 @@ def detection_callback(device, advertisement_data):
         "mac" : device.address,
         "Color" : tiltcolordict.get(adv.uuid),
         "timeStamp" : time.time() * 1000  ,
-        "formatteddate" : datetime.datetime.now().strftime('%x %X'),
+        "formatteddate" : formatteddate,
         "Timepoint" : (time.time() / 60 / 60 / 24 + 25569) - time.timezone / 60 / 60 / 24,
         "uncalSG" : uncalSG,
         "uncalTemp" : uncalTemp,
         "battAgeWeeks" : battAgeWeeks,
         "opStatus" : opStatus,
         }
-       if tiltdatalist == []:
-        tiltdatalist.append(tiltdatadict)
-       for i in tiltdatalist:
-        if i['mac'] == tiltdatadict['mac']:
-          tiltdatalist[tiltdatalist.index(i)] = tiltdatadict
-        elif tiltdatalist.index(i) == len(tiltdatalist) - 1:
+        global tiltdatalist
+        if len(tiltdatalist) == 0:
           tiltdatalist.append(tiltdatadict)
-       print(tiltdatalist)
-       
-
-event_control = asyncio.Event()
+        for i in tiltdatalist:
+          if i['mac'] == tiltdatadict['mac']:
+            tiltdatalist[tiltdatalist.index(i)] = tiltdatadict
+          elif tiltdatalist.index(i) == len(tiltdatalist) - 1:
+            tiltdatalist.append(tiltdatadict)
+        #print(len(tiltdatalist))
+        # log scanned tilt info to csv every 15 minutes
+        for i in tiltdatalist:
+          if i['timeStamp'] + 900000 < time.time() * 1000:
+            try:
+              with open(('TILT-' + (str(tiltcolordict.get(adv.uuid)) + '-' + device.address).replace(':','-')) + '.csv','x') as f:
+                f.write(
+                    'Timestamp,Timepoint,SG,Temp,Color,Beer,Comment\n' 
+                  + formatteddate + ','
+                  + str(Timepoint) + ','
+                  + str(uncalSG) + ','
+                  + str(uncalTemp) + ','
+                  + tiltcolordict.get(adv.uuid) + ':' + device.address + ','
+                  + 'Untitled' + ','
+                  + '' + '\n'
+                  )
+            except:
+              with open(('TILT-' + (str(tiltcolordict.get(adv.uuid)) + '-' + device.address).replace(':','-')) + '.csv','a') as f:
+                f.write(
+                    formatteddate + ','
+                  + str(Timepoint) + ','
+                  + str(uncalSG) + ','
+                  + str(uncalTemp) + ','
+                  + tiltcolordict.get(adv.uuid) + ':' + device.address + ','
+                  + 'Untitled' + ','
+                  + '' + '\n'
+                  )
 
 async def startWebServer():
-  server = web.Server(handler)
-  runner = web.ServerRunner(server)
-  await runner.setup()
-  site = web.TCPSite(runner, 'localhost', 1880)
-  await site.start()
-  print("======= Serving on http://127.0.0.1:8080/ ======")
+  server1 = web.Server(handler1)
+  server2 = web.Server(handler2)
+  runner1 = web.ServerRunner(server1)
+  runner2 = web.ServerRunner(server2)
+  await runner1.setup()
+  await runner2.setup()
+  site1 = web.TCPSite(runner1, 'localhost', 1880)
+  site2 = web.TCPSite(runner2, 'localhost', 1881)
+  await site1.start()
+  await site2.start()
+  print("======= Serving on http://127.0.0.1:1880/ ======")
+  print("======= Serving on http://127.0.0.1:1881/ ======")
   await event_control.wait()
 
 async def startTiltScanner():
